520198f79ca12a1d6960b98627231311cc9d2916
ar2r91 <theoithy@gmail.com>
2017-02-01 12:18:01 -0500

    1.9 subo solo codigo

--- app/Http/Controllers/donacionController.php
+++ app/Http/Controllers/donacionController.php
@@ -15,15 +15,14 @@ class donacionController extends Controller
         $donacion->setFechaIngreso($request->fechaIngreso);
         $donacion->setDescripcion($request->descripcion);
         $donacion->setMonto($request->monto);
-        $nombreT = $request ->nombreTramite;
+        $nombreT = $request->nombreTramite;
         $idD = $donacion->bdTramite($nombreT);
         $donacion->setIdTramite($idD);
-        $don= $donacion->save();
+        $don = $donacion->save();
 
-        if($don==true) {
+        if ($don == true) {
             return view('Administrador/DonacionesYTransacciones/Add');
-        }
-        else{
+        } else {
             return view('Administrador/DonacionesYTransacciones/Add');
         }
     }
@@ -31,49 +30,59 @@ class donacionController extends Controller
     public function autocompletet(Request $request)
     {
         $data = DB::table('tramite')->select("nombre as name")->where("nombre", "LIKE", "%{$request->input('query')}%")->get();
-
         return response()->json($data);
     }
 
-    public function cargarDonacion($codTramite)
+    public function cargarDonacion($codDonacion)
     {
         $donacion = new donacionmodel();
-        $tra = $donacion->consultarTramiteid($codTramite);
-        return view('Administrador/DonacionesYTransacciones/Edit')->with(['tramite' => $tra]);
+        $don = $donacion->consultarDonacionid($codDonacion);
+        return view('Administrador/DonacionesYTransacciones/Edit')->with(['donacion' => $don]);
     }
 
-    public function editarDonacion($codTramite, Request $request)
+    public function editarDonacion($codDonacion, Request $request)
     {
         $donacion = new donacionmodel();
-        $donacion->setNombre($request->numeroResolucion);
-        $donacion->setFuentefinanc($request->fechaDeIngreso);
-        $donacion->setTipoRecurso($request->monto);
-        $donacion->setTipoRecurso($request->descripcion);
-        $donacion->editarTramite($codTramite);
-        return view('Administrador/DonacionesYTransacciones/Search')->with(['nombre' => $request->nombreTramite]);
+        $donacion->setNumResolucion($request->numeroResolucion);
+        $donacion->setDescripcion($request->descripcion);
+        $donacion->setMonto($request->monto);
+        $donacion->setFechaIngreso($request->fechaDeIngreso);
+        $donacion->editarDonacion($codDonacion);
+        return view('Administrador/DonacionesYTransacciones/Search')->with(['nombre' => $request->numeroResolucion]);
     }
-
+    //Falta buscar desde una fecha hasta otra fecha....
     public function listarDonacion(Request $request)
     {
         $don = null;
         $donacion = new donacionmodel();
 
-        if ($request->select == 'Clasificador Siaf') {
-            $don = $donacion->consultarTramiteCS($request->text);
+        if ($request->select == 'Fecha') {
+            $don = $donacion->consultarDonacionFecha($request->text);
         } else {
-            if ($request->select == 'Tipo de recurso') {
-                $don = $donacion->consultarTramiteTR($request->text);
+            if ($request->select == 'Codigo Siaf') {
+                $don = $donacion->consultarDonacionCodigoSiaf($request->text);
             } else {
-                if ($request->select == 'Nombre de tramite') {
-                    $don = $donacion->consultarTramiteN($request->text);
+                if ($request->select == 'Tipo de recurso') {
+                    $don = $donacion->consultarDonacionTipoRecurso($request->text);
                 } else {
                     if ($request->select == 'Fuente de financiamiento') {
-                        $don = $donacion->consultarTramiteFF($request->text);
+                        $don = $donacion->consultarDonacionFuenteFinanciamiento($request->text);
+                    } else {
+                        if ($request->select == 'Numero Resolucion') {
+                            $don = $donacion->consultarDonacionNumeroResolucion($request->text);
+                        }
                     }
                 }
             }
         }
         return view('Administrador/DonacionesYTransacciones/Search')->with(['donacion' => $don, 'txt' => $request->text, 'select' => $request->select]);
+    }
+
+    public function eliminarTramite($codDonacion, Request $request)
+    {
+        $donacion = new donacionmodel();
+        $donacion->eliminarDonacion($codDonacion);
+        return view('Administrador/DonacionesYTransacciones/Search')->with(['nombre' => $request->numeroResolucion]);
     }
 
 }
